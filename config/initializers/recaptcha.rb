603b1fb55ca275889cda77b9c011ca7e17280a68
littleali <ali.nosrati72@gmail.com>
2015-02-19 13:33:40 +0330

    CAPTCHA added to sign up form

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,9 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6Lc_VAITAAAAAGadf55za3I4G4BvoYySk4oMo2Jw'
+  config.private_key = '6Lc_VAITAAAAADkNwQHXXJJQ9ptPu-QmABMm1eXr'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+  # Uncomment if you want to use the newer version of the API,
+  # only works for versions >= 0.3.7:
+  # config.api_version = 'v2'
+end
