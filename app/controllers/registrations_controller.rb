9c6f276f1a71da4d21e9c0501e06117b8e8a5216
mzhariann <mzh.ariann@gmail.com>
2015-02-20 21:46:10 +0330

    Alert fixed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
     else
       build_resource(sign_up_params)
       clean_up_passwords(resource)
-      flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."      
+      flash.now[:alert] = "لطفا نوشته را دوباره وارد کنید."      
       flash.delete :recaptcha_error
       render :new
     end
--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -19,10 +19,8 @@ module DeviseHelper
                       :resource => resource.class.model_name.human.downcase)
 
     html = <<-HTML
-    <div id="error_explanation">
       <h2>#{sentence}</h2>
       <ul>#{messages}</ul>
-    </div>
     HTML
 
     html.html_safe
