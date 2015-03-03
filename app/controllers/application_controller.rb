238d7f9a5aac6afef4eefb1b3f17f4793a43a2f7
Noid <navid.saleh.n@gmail.com>
2015-03-03 19:23:45 +0330

    add navbar and footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,23 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
+
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:signin,:username, :email, :password, :remember_me)}
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :username, :password, :password_confirmation, :fname, :lname, :username, :bdate)}
   end
+
+  def layout_by_resource
+    if devise_controller?
+      'devise'
+    else
+      'application'
+    end
+  end
+
   def after_sign_in_path_for(resource)
     root_path
   end
