728dbcd1ea49ab37f17a604eaf4ed76fdca83338
littleali <ali.nosrati72@gmail.com>
2015-03-06 17:00:59 +0330

    Admin added to players in creation of room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -26,6 +26,7 @@ class RoomsController < ApplicationController
   def create
     @room = Room.new(room_params)
     @room.admin = current_user
+    @room.players << current_user
     respond_to do |format|
       if @room.save
         format.html { redirect_to @room, notice: 'Room was successfully created.' }
