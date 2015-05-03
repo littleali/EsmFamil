ce7030f437f782298b3f19af4ec7e81b435fb456
Yasaman Razeghi <Yasaman@Gol-Anar.local>
2015-05-03 18:08:40 +0430

    inline edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,3 +40,37 @@ $('.best_in_place').bind("ajax:success", function () {$(this).closest('tr').effe
 
 client = new Faye.Client('/faye');
 window.client = client;
+//
+//$(function() {
+//    $('span.best_in_place').each(function() {
+//        var attrs, el;
+//        el = $(this);
+//        attrs = el.data('html-attrs');
+//        if (attrs && attrs['tabindex']) {
+//            el.attr('tabindex', attrs['tabindex']);
+//        }
+//    }).focus(function() {
+//        var el;
+//        el = $(this);
+//        el.click();
+//    });
+//});
+
+
+
+$(function() {
+    $('span.best_in_place[data-bip-html-attrs]').each(function() {
+        var attrs, el;
+        el = $(this);
+        attrs = el.data('bip-html-attrs');
+        if (attrs && attrs['tabindex']) {
+            el.attr('tabindex', attrs['tabindex']);
+        }
+        el.click();
+    }).focus(function() {
+        var el;
+        el = $(this);
+        el.click();
+    });
+});
+
