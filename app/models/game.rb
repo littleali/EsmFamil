75dd350afd198ddef522d18de028bd61f5b034e7
littleali <ali.nosrati72@gmail.com>
2015-05-03 08:19:25 +0430

    redirect all who see page

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,28 +2,30 @@ class Game
   include Mongoid::Document
   field :title, type: String
   field :start_time, type: DateTime
+  field :stopped, type: Boolean
   belongs_to :room, class_name: 'Room', inverse_of: :games
   has_many :papers , class_name: 'Paper' , inverse_of: :game
 
   validates :title, :uniqueness => {:scope => :room_id}
   def is_starting
-  	if self.start_time and not self.is_finished and not self.is_runing
+  	if self.start_time and not self.is_stopped and not self.is_runing
   		return true
   	end
   	return false
   end
 
   def is_runing
-  	if self.start_time and not DateTime.now < start_time
+  	if self.start_time and not DateTime.now < start_time and not is_stopped
   		return true
   	end
   	return false
   end
 
-  def is_finished
-  	if self.start_time and self.start_time + 1.minute < DateTime.now
-  		return true
-  	end
-  	return false
+  def stop
+    self.stopped = true
+  end
+
+  def is_stopped
+    self.stopped
   end
 end
