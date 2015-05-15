908571bb3cb38011eb181aa9c0b7ca057882e0c3
littleali <ali.nosrati72@gmail.com>
2015-05-15 19:36:52 +0430

    add ajax bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -34,8 +34,8 @@ Rails.application.routes.draw do
   root 'welcome#home'
 
   patch 'rooms/:id/add_member/:profile_id' => 'rooms#add_member'
-  delete 'rooms/:id/kick_out/:profile_id' => 'rooms#kick_out'
-  delete 'rooms/:id/leave' => 'rooms#leave'
+  delete 'rooms/:id/kick_out/:profile_id' => 'rooms#kick_out' 
+  delete 'rooms/:id/leave' => 'rooms#leave', as: :leave_room
    # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
