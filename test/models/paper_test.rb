9aee09a3a9d139795aa3bb1adf5105b63aeae44a
Yasaman Razeghi <Yasaman@Gol-Anar.local>
2015-05-16 11:41:06 +0430

    model test

--- test/models/paper_test.rb
+++ test/models/paper_test.rb
@@ -4,4 +4,18 @@ class PaperTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "paper field" do
+    game = Game.new(:title => 'test3')
+    assert game.save
+    paper = Paper.new
+    paper.game = game
+    paper.save
+    assert_equal(paper.paper_fields.count, game.item_names.length)
+    paper.paper_fields.each do  |pf|
+      assert game.item_names.include? pf.name
+    end
+    paper.destroy
+    game.destroy
+  end
+
 end
