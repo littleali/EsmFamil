a7b00246f4c8538fa69320b1058e1c30a96d6131
mzhariann <mzh.ariann@gmail.com>
2015-05-29 14:43:37 +0430

    private room

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,6 +3,7 @@ class Room
   field :name, type: String
   field :capacity, type: Integer
   field :enabled, type: Mongoid::Boolean
+  field :is_private, type: Boolean, default: false
   has_and_belongs_to_many :players , class_name: 'Profile' , inverse_of: :playing_rooms
   belongs_to :admin, class_name: 'Profile', inverse_of: :owned_rooms
   validates_uniqueness_of :name
